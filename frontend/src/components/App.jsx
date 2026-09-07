336aea8d3b215f3b8b47b094193621ae5a35b09e
Yazid Boestami Mohd Yusoff <yazidb@Yazids-MacBook-Pro.local>
2026-09-07 21:52:49 +0800

    added add-note function

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes((prevNote) => {
+      return [...prevNote, newNote];
+    });
+  }
+
   return (
     <div>
       <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+      <CreateArea onAdd={addNote} />
+      {notes.map((note, index) => {
+        return (
+          <Note
+            key={index}
+            index={index}
+            title={note.title}
+            content={note.content}
+          />
+        );
+      })}
       <Footer />
     </div>
   );
--- frontend/src/components/CreateArea.jsx
+++ frontend/src/components/CreateArea.jsx
@@ -1,10 +1,44 @@
-function CreateArea() {
+import { useState } from "react";
+
+function CreateArea(props) {
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  });
+
+  function handleInput(event) {
+    const { name, value } = event.target;
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value,
+      };
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    props.onAdd(note);
+    setNote({ title: "", content: "" });
+  }
+
   return (
     <div>
-      <form className="create-note">
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+      <form className="create-note" onSubmit={handleSubmit}>
+        <input
+          name="title"
+          value={note.title}
+          placeholder="Title"
+          onChange={handleInput}
+        />
+        <textarea
+          name="content"
+          value={note.content}
+          placeholder="Take a note..."
+          onChange={handleInput}
+          rows="3"
+        />
+        <button type="submit">Add</button>
       </form>
     </div>
   );
