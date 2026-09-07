53a5fd99f25224a9bcf47b2b099c94f5ae61f745
Yazid Boestami Mohd Yusoff <yazidb@Yazids-MacBook-Pro.local>
2026-09-07 21:01:04 +0800

    change from ReactDOM to createRoot to render

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,8 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const container = document.getElementById("root");
+
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
 
 //CHALLENGE:
 //1. Implement the add note functionality.
