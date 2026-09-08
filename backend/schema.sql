458398892c7f02ffb0dbe1a06ff923669ad32cdd
Yazid Boestami Mohd Yusoff <yazidb@Mac.dlinkrouter.local>
2026-09-08 17:36:19 +0800

    added password_hash column for non-google user

--- backend/schema.sql
+++ backend/schema.sql
@@ -3,7 +3,8 @@ DROP TABLE IF EXISTS users;
 
 CREATE TABLE IF NOT EXISTS users (
   id         SERIAL PRIMARY KEY,
-  google_id  VARCHAR(255) UNIQUE NOT NULL,
+  google_id  VARCHAR(255) UNIQUE,
+  password_hash VARCHAR(255),
   email      VARCHAR(255) UNIQUE NOT NULL,
   name       VARCHAR(255),
   created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
@@ -17,6 +18,11 @@ CREATE TABLE IF NOT EXISTS notes (
 );
 
 CREATE INDEX idx_notes_user_id ON notes (user_id);
+
+ALTER TABLE users
+  ADD CONSTRAINT chk_auth_method
+  CHECK (google_id IS NOT NULL OR password_hash IS NOT NULL);
+
 
 -- adding test-user on the table
 INSERT INTO users (google_id, email, name)
