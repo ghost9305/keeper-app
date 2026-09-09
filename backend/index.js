aa05149417729260e6f3a865c82d196835f03807
Yazid Boestami Mohd Yusoff <yazidb@Yazids-MacBook-Pro.local>
2026-09-09 14:09:57 +0800

    added an error handler middleware at the bottom of index.js

--- backend/index.js
+++ backend/index.js
@@ -42,6 +42,11 @@ app.delete("/notes/:id", async (req, res) => {
   res.status(204).send();
 });
 
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({ error: "internal server error" });
+});
+
 app.listen(Number(process.env.PORT), () => {
   console.log(`Server is running on port ${Number(process.env.PORT)}`);
 });
