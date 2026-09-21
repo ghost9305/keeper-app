21b05b6bfe2b3a4f3060a48b7099209134c9b10d
Yazid Boestami Mohd Yusoff <yazidb@Yazids-MacBook-Pro.local>
2026-09-21 13:57:44 +0800

    add some sql in schema.sql

--- backend/schema.sql
+++ backend/schema.sql
@@ -36,3 +36,17 @@ VALUES (
     'Test Note',
     'This is test content for local dev.'
 );
+
+
+-- table.sql from connect-pg-simple --
+-- automatically created with createIfTableMissing option while setting up session
+CREATE TABLE IF NOT EXISTS "session" (
+  "sid" varchar NOT NULL COLLATE "default",
+  "sess" json NOT NULL,
+  "expire" timestamp(6) NOT NULL
+)
+WITH (OIDS=FALSE);
+
+ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
+
+CREATE INDEX IF NOT EXISTS "IDX_session_expire" ON "session" ("expire");
