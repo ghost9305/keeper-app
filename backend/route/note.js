414d1fd711043844c60cb794ff497c9124a4f130
Yazid Boestami Mohd Yusoff <yazidb@Yazids-MacBook-Pro.local>
2026-09-21 11:07:46 +0800

    mount ensureAuth.js on route/note.js

--- backend/route/note.js
+++ backend/route/note.js
@@ -1,14 +1,17 @@
 import express from "express";
 import { pool } from "../config/db.js";
+import { ensureAuth } from "../middleware/ensureAuth.js";
 
 const router = express.Router();
 
-const test_user_id = 1;
+router.use(ensureAuth);
+
+// const test_user_id = 1;
 
 router.get("/", async (req, res) => {
   const result = await pool.query(
     "SELECT id, title, content FROM notes WHERE user_id = $1 ORDER BY id ASC",
-    [test_user_id],
+    [req.user.id],
   );
   res.status(200).json(result.rows);
 });
@@ -20,7 +23,7 @@ router.post("/", async (req, res) => {
   }
   const result = await pool.query(
     "INSERT INTO notes (user_id, title, content) VALUES ($1,$2,$3) RETURNING id, title, content",
-    [test_user_id, title, content],
+    [req.user.id, title, content],
   );
   res.status(201).json(result.rows[0]);
 });
@@ -29,7 +32,7 @@ router.delete("/:id", async (req, res) => {
   const { id } = req.params;
   const result = await pool.query(
     "DELETE FROM notes WHERE id = $1 AND user_id = $2 RETURNING id",
-    [id, test_user_id],
+    [id, req.user.id],
   );
   if (result.rowCount === 0) {
     return res.status(404).json({ error: "note not found!" });
